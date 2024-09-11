2fed2b290addc9febde119514e9b32eb13e38689
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-11 19:00:02 -0400

    add explicit GET in routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,14 +10,14 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /static/", http.FileServerFS(view.Files))
-	mux.HandleFunc("/", app.getIndex)
+	mux.HandleFunc("GET /", app.getIndex)
 
-	mux.HandleFunc("/dashboard", app.getDashboard)
-	mux.HandleFunc("/dashboard/new-account", app.getNewAccount)
+	mux.HandleFunc("GET /dashboard", app.getDashboard)
+	mux.HandleFunc("GET /dashboard/new-account", app.getNewAccount)
 	mux.HandleFunc("POST /dashboard/accounts", app.postNewAccount)
 
-	mux.HandleFunc("/login", app.getLogin)
-	mux.HandleFunc("/register", app.getRegister)
+	mux.HandleFunc("GET /login", app.getLogin)
+	mux.HandleFunc("GET /register", app.getRegister)
 
 	return mux
 }
