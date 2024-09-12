db1ebb05ee1f9ac4f2f9310e3844fe97528321eb
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-12 18:20:04 -0400

    add users table migration

---
+++ migrations/000003_add_users_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT NOT NULL,
+    email TEXT NOT NULL, 
+    hashed_password TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT current_timestamp,
+    updated_at DATETIME NOT NULL DEFAULT current_timestamp
+);
