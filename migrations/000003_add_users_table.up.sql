a3941f86dba3b29f9ed5b002aca6dcad99c25b1f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-12 18:48:20 -0400

    add constraint to email column

--- migrations/000003_add_users_table.up.sql
+++ migrations/000003_add_users_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL, 
     hashed_password TEXT NOT NULL,
     created_at DATETIME NOT NULL DEFAULT current_timestamp,
-    updated_at DATETIME NOT NULL DEFAULT current_timestamp
+    updated_at DATETIME NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT email_unique UNIQUE (email)
 );
