e05bb7451ff7a14d949156f1d267973cb16bfc8a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-12 22:17:58 -0400

    protected dashboard routes to authorized users only

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -25,3 +25,7 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+}
